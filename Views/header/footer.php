8cbf118f5509d7b8d4670b0183fdf23fd2977d89
Fernando <fernandito1606@hotmail.com>
2024-08-11 11:43:37 +0200

    primer intento

---
+++ Views/header/footer.php
@@ -0,0 +1,26 @@
+</html>
+<body>
+ <div class="container" style="background-color: #fff; border-radius: 5px; padding: 7px; margin-top:20px; width: 1140px; margin-left: 8.3%;">
+
+<footer>
+	<center><small><i>Copyright (c) Polleria <?php echo date('Y'); ?> <a href="" target="_blanck">Rico Pollo</a></i></small></center>
+</footer>
+</div>
+
+	<script src="assets/js/script.js"></script>
+
+<script>
+ 
+$('.mi').on('click',function(){
+	$('.su').toggle('slow');
+});
+
+$('.mitooltip').tooltip();
+  $( function() {
+  
+  });
+
+</script>
+   
+</body>
+</html>
---
+++ Views/modules/ventas/pdf/lista.php
@@ -0,0 +1,52 @@
+<?php 
+	require_once("dompdf/dompdf_config.inc.php");
+	$conexion = mysql_connect("localhost","root","");
+	mysql_select_db("tarea",$conexion);
+
+$codigoHTML='
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "">
+<html xmlns="">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Lista</title>
+</head>
+
+<body>
+<div align="center">
+	<img src="img/alumno.png" width="100px" height="100px">
+    <table width="95%" border="1">
+      <tr>
+        <td bgcolor="#0099FF"><strong>Nit</strong></td>
+        <td bgcolor="#0099FF"><strong>Nombre</strong></td>
+        <td bgcolor="#0099FF"><strong>Direccion</strong></td>
+        <td bgcolor="#0099FF"><strong>Correo</strong></td>
+        <td bgcolor="#0099FF"><strong>Telefono</strong></td>
+        <td bgcolor="#0099FF"><strong>Ocupacion</strong></td>
+      </tr>';
+
+        $consulta=mysql_query("SELECT * FROM empleados");
+        while($dato=mysql_fetch_array($consulta)){
+$codigoHTML.='
+      <tr>
+        <td>'.$dato['id'].'</td>
+        <td>'.$dato['nombre'].'</td>
+        <td>'.$dato['direccion'].'</td>
+        <td>'.$dato['correo'].'</td>
+        <td>'.$dato['telefono'].'</td>
+        <td>'.$dato['ocupacion'].'</td>
+      </tr>';
+      } 
+$codigoHTML.='
+    </table>
+	<img src="img/alumno.png" width="100px" height="100px">
+</div>
+</body>
+</html>';
+
+$codigoHTML=utf8_decode($codigoHTML);
+$dompdf=new DOMPDF();
+$dompdf->load_html($codigoHTML);
+ini_set("memory_limit","128M");
+$dompdf->render();
+$dompdf->stream("ListadoEmpleado.pdf");
+?>
